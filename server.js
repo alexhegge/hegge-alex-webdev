c0b3b0add0666cf02e1189d5c90e3dd1857bb223
[Alex Hegge] <Alex@Alexs-MacBook-Pro.local>
2017-07-11 23:10:51 -0400

    Second commit

--- server.js
+++ server.js
@@ -11,4 +11,11 @@ app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+
+
+//app.listen(3000);
+
+
